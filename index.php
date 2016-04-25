2adb8d7ea1a04ff00153b330bf5e2f697e949b6e
emaestro <io@MacBook-Pro-de-Fredy.local>
2016-04-25 17:41:26 -0500

    Implementamos la opcion listado

--- index.php
+++ index.php
@@ -1,21 +1,48 @@
 <?php
 	include('inc/cabecera.php');
 	include('inc/menu.php');
+
+	include("inc/db.php");
+
+	$resultado = $conexion->query("SELECT p.* FROM personas p");
+
 ?>
 
 <div class="container">
 
 	<div class="row">
 		<div class="col-md-8">
-			<h1>Inicio</h1>
-			<div class="alert alert-info">
-				Columna de 8 partes
-			</div>	
+			<h1>CRUD Personas</h1>
+			<table class='table table-condensed'>
+				<tr>
+					<th>Id</th>
+					<th>Paterno</th>
+					<th>Materno</th>
+					<th>Nombres</th>
+					<th></th>
+				</tr>
+<?php
+				while ( $fila = $resultado->fetch_assoc() )
+				{
+?>
+					<tr>
+						<td><?php echo $fila['personas_id']?></td>
+						<td><?php echo $fila['paterno']?></td>
+						<td><?php echo $fila['materno']?></td>
+						<td><?php echo $fila['nombres']?></td>
+						<td> <a href="modificar.php?id=<?php echo $fila['personas_id']?>"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> </td>
+						<td> <a href="eliminar.php?id=<?php echo $fila['personas_id']?>"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a> </td>
+					</tr>
+<?php
+				}
+?>
+
+			</table>
 		</div>
 		<div class="col-md-4">
-		<div class="alert alert-success">
-			Columna de 4 partes
-		</div>
+			<div class="alert alert-success">
+				<strong>Bienvenidos</strong> al CRUD Personas.
+			</div>
 		</div>
 	</div>
 
