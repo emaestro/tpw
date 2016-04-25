46b702fba1d03b9ac23294a114fb90b6e2ac29cd
emaestro <io@MacBook-Pro-de-Fredy.local>
2016-04-25 17:54:38 -0500

    Agregamos el Formulario Nueva Persona

---
+++ personas_nuevo.php
@@ -0,0 +1,59 @@
+<?php
+	include('inc/cabecera.php');
+	include('inc/menu.php');
+
+	include("inc/db.php");
+
+	$resultado = $conexion->query("SELECT p.* FROM personas p");
+
+?>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-8">
+
+			<h1>Nueva Persona</h1>
+
+			<form method="post"action="personas_nuevo_guarda.php"  class="form-horizontal">
+				<div class="form-group">
+					<label for="txtPaterno" class="col-sm-2 control-label">Paterno</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="txtPaterno" placeholder="Apellido paterno" name="txtPaterno">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="txtMaterno" class="col-sm-2 control-label">Materno</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="txtMaterno" placeholder="Apellido materno" name="txtMaterno">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="txtNombres" class="col-sm-2 control-label">Nombres</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="txtNombres" placeholder="Nombres" name="txtNombres">
+					</div>
+					<input type="submit"name="btnGuardar"value="Guardar">
+				</div>
+				Paterno<br>
+				<input type="text"name="txtPaterno"><br>
+				Materno<br>
+				<input type="text"name="txtMaterno"maxlength="30"><br>
+				Nombres<br>
+				<input type="text"name="txtNombres"><br>
+				<input type="submit"name="btnGuardar"value="Guardar">
+			</form>
+		</div>
+		<div class="col-md-4">
+			<div class="alert alert-success">
+				<strong>Bienvenidos</strong> al CRUD Personas.
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<?php
+	include('inc/scripts.php');
+	include('inc/pie.php');
+?>
