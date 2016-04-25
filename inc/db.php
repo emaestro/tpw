257a5fecbab0ccdbf2de8fe2ad92b21293ccba93
emaestro <io@MacBook-Pro-de-Fredy.local>
2016-04-25 17:19:33 -0500

    Creamos la configuracion de la BBDD

---
+++ inc/db.php
@@ -0,0 +1,11 @@
+<?php 
+	$host = "localhost";
+	$user = "root";
+	$password = "root";
+	$database = "programacion_web";
+	$conexion = new mysqli($host, $user, $password, $database);
+	if( $conexion->connect_errno)
+	{
+		echo "<h3>Error de conexion a la BD</h3>";
+	}
+?>
