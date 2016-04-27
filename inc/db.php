ef8956863e682129e65e8b00b6c65124c6c5be09
emaestro <f@emaestro.net>
2016-04-27 17:22:11 -0500

    Cambios en db.php

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,15 @@
 <?php 
-	$host = "localhost";
-	$user = "root";
-	$password = "root";
-	$database = "programacion_web";
+
+	echo getenv('OPENSHIFT_MYSQL_DB_HOST');
+	echo "<br>";
+	echo getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	echo "<br>";
+	echo getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	echo "<br>";
+	echo getenv('OPENSHIFT_MYSQL_DB_PORT');
+	echo "<br>";
+	echo getenv('OPENSHIFT_APP_NAME');
+
 
 	$conexion = new mysqli($host, $user, $password, $database);
 	
